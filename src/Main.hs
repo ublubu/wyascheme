912aeeb94b7e1ebac1fc574a7d3ef2ee0845ca24
Kynan Rilee <kynan.rilee@gmail.com>
2016-02-19 00:10:19 -0800

    simple repl

--- src/Main.hs
+++ src/Main.hs
@@ -2,18 +2,43 @@
 module Main where
 
 import Control.Monad.Except
+import Data.Either.Combinators
+import System.IO
 import Text.ParserCombinators.Parsec (parse)
+
 import Types
 import Parse
 import Eval
+
+flushStr :: String -> IO ()
+flushStr s = putStr s >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
 
 readExpr :: (MonadError LispError m) => String -> m LispVal
 readExpr input = case parse parseExpr "lisp" input of
   Left err -> throwError . Parser $ err
   Right val -> return val
 
-test :: String -> IO ()
-test = print . runExcept . (eval <=< readExpr)
+evalString :: String -> Either LispError LispVal
+evalString = runExcept . (eval <=< readExpr)
+
+evalToString :: String -> String
+evalToString = uneither . mapBoth show show . evalString
+
+uneither :: Either a a -> a
+uneither (Left x) = x
+uneither (Right x) = x
+
+evalAndPrint :: String -> IO ()
+evalAndPrint = putStrLn . evalToString
+
+repl :: IO ()
+repl = do
+  expr <- readPrompt "Lisp>>> "
+  case expr of "quit" -> return ()
+               _ -> evalAndPrint expr >> repl
 
 main :: IO ()
-main = putStrLn "hello"
+main = repl
